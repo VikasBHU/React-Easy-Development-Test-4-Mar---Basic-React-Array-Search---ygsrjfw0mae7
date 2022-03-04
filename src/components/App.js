522abc4eb7be15a81411eec23700a01723632360
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-03-04 19:10:23 +0530

    ReactEasyProctored

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta"
+];
 const App = () => {
+  for (let i = 0; i < searchArray.length; i++) {}
+  function Btn() {
+    const [Button, setButton] = useState();
+  }
 
-  
   return (
     <div id="main">
-      
+      <input type="text" onClick={App} placeholder="Data" />
+      <select
+        options={searchArray}
+        onChange={(options) => console.log(options.input)}
+      />
+      <button onClick={Btn}>Search</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
